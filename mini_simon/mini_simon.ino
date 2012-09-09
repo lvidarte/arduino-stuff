f14c046de6f74b5eb3d5b00a9ef007a52283aeba
Leonardo Vidarte <lvidarte@gmail.com>
2012-09-09 20:10:36 -0300

    Little refactoring on mini Simon

--- mini_simon/mini_simon.ino
+++ mini_simon/mini_simon.ino
@@ -1,31 +1,34 @@
 /* vim: set ft=c : */
 
 /**
+ *  Mini version of Simon (1978)
+ *
  *  Connect leds on 8, 10, 12
  *  Connect switches on 2, 4, 6
  */
 
-const int buttons[3] = {2, 4, 6};
-const int leds[3] = {8, 10, 12};
+const int totalLeds = 3;
+const int leds[totalLeds] = {8, 10, 12};
+const int buttons[totalLeds] = {2, 4, 6};
 
 boolean done;
-boolean gameover = false;
+boolean gameOver = false;
 
-const int len = 5;
-int seq[len];
-int actual = 0;
+const int seqLength = 5;
+int seq[seqLength];
+int seqIndex = 0;
 
 
 void setup() {
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < totalLeds; i++) {
         pinMode(leds[i], OUTPUT);
     }
 
     /* Gen and show sequence */
     randomSeed(analogRead(0));
-    for (int i = 0; i < len; i++) {
-        seq[i] = random(3);
+    for (int i = 0; i < seqLength; i++) {
+        seq[i] = random(totalLeds);
         digitalWrite(leds[seq[i]], HIGH);
         delay(500);
         digitalWrite(leds[seq[i]], LOW);
@@ -37,15 +40,15 @@ void setup() {
 void loop() {
 
     // Game
-    if (!gameover && actual < len) {
-        for (int i = 0; i < 3; i++) {
+    if (!gameOver && seqIndex < seqLength) {
+        for (int i = 0; i < totalLeds; i++) {
             done = false;
             while(digitalRead(buttons[i]) == HIGH) {
                 if (!done) {
                     done = true;
                     digitalWrite(leds[i], HIGH);
-                    if (seq[actual++] != i) {
-                        gameover = true;
+                    if (seq[seqIndex++] != i) {
+                        gameOver = true;
                     }
                 }
             }
@@ -54,7 +57,7 @@ void loop() {
     }
 
     // You Win
-    if (!gameover && actual == len) {
+    if (!gameOver && seqIndex == seqLength) {
         delay(50);
         digitalWriteAll(HIGH);
         delay(50);
@@ -62,8 +65,8 @@ void loop() {
     }
 
     // Game Over
-    if (gameover) {
-        delay(200);
+    if (gameOver) {
+        delay(50);
         digitalWriteAll(HIGH);
     }
 
@@ -72,7 +75,7 @@ void loop() {
 }
 
 void digitalWriteAll(int value) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < totalLeds; i++) {
         digitalWrite(leds[i], value);
     }
 }

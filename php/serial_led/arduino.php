c8c8907886f03aaf7d5f4b960de8552db4653603
Leonardo Vidarte <lvidarte@gmail.com>
2013-04-30 10:12:46 -0300

    Refactoring on arduino.php

--- php/serial_led/arduino.php
+++ php/serial_led/arduino.php
@@ -3,11 +3,11 @@ if (isset($_GET['action'])) {
     require_once('php_serial.class.php');
 
     $action = $_GET['action'];
-    $msec = isset($_GET['msec']) ? $_GET['msec'] : 250000;
-    $times = isset($_GET['times']) ? $_GET['times'] * 2 : 10;
+    $msec = isset($_GET['msec']) ? $_GET['msec'] : 500000;
+    $times = isset($_GET['times']) ? $_GET['times'] : 5;
 
     $serial = new phpSerial();
-    $serial->deviceSet('/dev/ttyUSB0');
+    $serial->deviceSet('/dev/ttyUSB1');
     $serial->confBaudRate(9600);
     $serial->deviceOpen();
 
@@ -17,9 +17,9 @@ if (isset($_GET['action'])) {
         $serial->sendMessage($actions[$action]);
     }
     if ($action == 'blink') {
-        for ($i = 1; $i <= $times; $i++) {
-            $j = ($i % 2 ? 'on' : 'off');
-            $serial->sendMessage($actions[$j]);
+        for ($i = 1; $i <= $times * 2; $i++) {
+            $_action = ($i % 2 ? 'on' : 'off');
+            $serial->sendMessage($actions[$_action]);
             usleep($msec);
         }
     }
@@ -30,13 +30,18 @@ if (isset($_GET['action'])) {
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Serial</title>
+    <title>Arduino LED control</title>
 </head>
 <body>
+    <h1>Arduino LED control</h1>
     <ul>
-        <li><a href="arduino.php?action=on" title="">On</a></li>
-        <li><a href="arduino.php?action=off" title="">Off</a></li>
-        <li><a href="arduino.php?action=blink&msec=250000&times=5" title="">Blink</a></li>
+        <li><a href="arduino.php?action=on">On</a></li>
+        <li><a href="arduino.php?action=off">Off</a></li>
+    </ul>
+    <h2>Blink</h2>
+    <ul>
+        <li><a href="arduino.php?action=blink">Slow</a></li>
+        <li><a href="arduino.php?action=blink&msec=100000&times=10">Fast</a></li>
     </ul>
 </body>
 </html>

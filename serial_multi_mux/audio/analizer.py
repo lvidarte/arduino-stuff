2f35b5b5bef4917effac04ec20e8f79f7920532f
Leonardo Vidarte <lvidarte@gmail.com>
2013-06-18 00:58:45 -0300

    Added new version of analizer

--- serial_multi_mux/audio/analizer.py
+++ serial_multi_mux/audio/analizer.py
@@ -1,19 +1,20 @@
 import wave
 import sys
 import struct
+from math import log10
 
 CHUNK = 256
+DBS = [-19, -13, -9, -7, -5, -3, -2, -1, 0]
 
 
 def open_wave(filename):
     return wave.open(filename, 'rb')
     #(nchannels, sampwidth, framerate, nframes, comptype, compname) = w.getparams()
 
-
 def data_analyzer(w, chunk=CHUNK):
     result = []
     while True:
-        data = w.readframes(CHUNK)
+        data = w.readframes(chunk)
         if not data:
             break
         values = struct.unpack("%dh" % (len(data) / 2), data)
@@ -23,30 +24,19 @@ def data_analyzer(w, chunk=CHUNK):
     return result
 
 def analyze_range(values):
-    out = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    max = 0
     for v in values:
-        v = abs(v)
-        if v < 3640:
-            out[0] += 1
-        elif v < 7280:
-            out[1] += 1
-        elif v < 10920:
-            out[2] += 1
-        elif v < 14560:
-            out[3] += 1
-        elif v < 18200:
-            out[4] += 1
-        elif v < 21840:
-            out[5] += 1
-        elif v < 25480:
-            out[6] += 1
-        elif v < 29120:
-            out[7] += 1
-        else:
-            out[8] += 1
-    print out
-    return max(out.iterkeys(), key=lambda k: out[k])
+        if v > max:
+            max = v
 
+    if max:
+        amplitude = max / 32767.0
+        dB = 20 * log10(amplitude)
+        for i, v in enumerate(DBS):
+            if dB <= v:
+                return i
+
+    return 0
 
 
 if __name__ == '__main__':
---
+++ serial_multi_mux/audio/vumeter2.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import random
+import Tkinter as tk
+import analizer
+
+WIDTH = 2
+HEIGHT = 8
+SIZE = 25
+SLEEP = 0.5
+
+class Application(tk.Frame):
+
+    def __init__(self, width=WIDTH, height=HEIGHT, size=SIZE):
+        tk.Frame.__init__(self, bg='black')
+        self.grid()
+        self.width = width
+        self.height = height
+        self.size = size
+        self.led_ids = []
+        self.led_colors = self.get_led_colors()
+        self.create_widgets()
+        self.draw_grid()
+        self.simulate()
+
+    def create_widgets(self):
+        width = self.width * self.size
+        height = self.height * self.size
+        self.canvas = tk.Canvas(self, width=width, height=height, bg='black')
+        self.canvas.grid(row=0, column=0, padx=5, pady=5)
+
+    def draw_grid(self):
+        for i in xrange(self.width - 1):
+            x = (self.size * i) + self.size
+            y0 = 0
+            y1 = self.size * self.height
+            self.canvas.create_line(x, y0, x, y1, fill='#333')
+        for i in xrange(self.height - 1):
+            x0 = 0
+            x1 = self.size * self.width
+            y = (self.size * i) + self.size
+            self.canvas.create_line(x0, y, x1, y, fill='#333')
+
+    def get_led_colors(self):
+        colors = []
+        for i in range(self.height):
+            percent = i * 100.0 / self.height
+            if percent < 50:
+                color = 'green'
+            elif percent < 75:
+                color = 'yellow'
+            else:
+                color = 'red'
+            colors.append(color)
+        return colors
+
+    def clear_vumeters(self):
+        for id in self.led_ids:
+            self.canvas.delete(id)
+        self.canvas.update()
+
+    def draw_vumeters(self, values):
+        self.clear_vumeters()
+        max_y1 = (self.height * self.size) - self.size
+        for i, v in enumerate(values):
+            x1 = i * self.size
+            x2 = x1 + self.size
+            for j in range(v):
+                y1 = max_y1 - (j * self.size)
+                y2 = y1 + self.size
+                id = self.canvas.create_rectangle(x1, y1, x2, y2,
+                                                  fill=self.led_colors[j],
+                                                  outline='#333')
+                self.led_ids.append(id)
+        self.canvas.update()
+
+    def simulate(self):
+        x = 256
+        y = 25
+        w = analizer.open_wave('test1.wav')
+        values = analizer.data_analyzer(w, x)
+        sleep_s = (x / 44100.0) * y
+        for i, v in enumerate(values):
+            if i % y == 0:
+                self.draw_vumeters(v)
+                time.sleep(sleep_s)
+
+if __name__ == '__main__':
+    prog = u'Vumeter'
+
+    from optparse import OptionParser
+    parser = OptionParser(description=prog)
+    parser.add_option('-v', '--vumeters', type=int, dest='width',
+                      default=WIDTH, help="Vumeters units")
+    parser.add_option('-l', '--leds', type=int, dest='height',
+                      default=HEIGHT, help="LEDs by vumeter")
+    parser.add_option('-s', '--size', type=int, default=SIZE,
+                      help="LEDs size")
+    args, _ = parser.parse_args()
+
+    app = Application(args.width, args.height, args.size)
+    app.master.title(prog)
+    app.mainloop()
+

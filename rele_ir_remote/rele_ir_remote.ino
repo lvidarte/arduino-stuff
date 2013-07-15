324df7d88551b2775e909d4ac7255b62a0bfec7d
Leonardo Vidarte <lvidarte@gmail.com>
2013-07-14 22:09:53 -0300

    Refactoring on rele ir remote

--- rele_ir_remote/rele_ir_remote.ino
+++ rele_ir_remote/rele_ir_remote.ino
@@ -1,45 +1,28 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
-#include <IRremote.h>
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
-int RECV_PIN = 11;
+/* vim: set ft=c : */
 
-IRrecv irrecv(RECV_PIN);
+#include <IRremote.h>
+
+int rele_pin = 13;
+int recv_pin = 11;
+int actual_state = LOW;
+
+IRrecv irrecv(recv_pin);
 decode_results results;
 
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);   
+void setup() {
+    pinMode(rele_pin, OUTPUT);
     Serial.begin(9600);
-    irrecv.enableIRIn(); // Start the receiver  
+    irrecv.enableIRIn(); // Start the receiver
 }
 
-int state = LOW;
-
-// the loop routine runs over and over again forever:
 void loop() {
-      if (irrecv.decode(&results)) {
+    if (irrecv.decode(&results)) {
         Serial.println(results.value, HEX);
-        if (results.value == 0xffa25d) {
-          state = (state == LOW) ? HIGH : LOW;
-          digitalWrite(led, state);
-          
+        if (results.value == 0xffa25d) { // On/Off button
+            actual_state = (actual_state == LOW) ? HIGH : LOW;
+            digitalWrite(rele_pin, actual_state);
         }
         irrecv.resume(); // Receive the next value
-      }
-      
-  /*    
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(10000);               // wait for a second
-*/
+    }
 }

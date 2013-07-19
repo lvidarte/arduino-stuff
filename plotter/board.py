0834ced13b99534b47a59a4f22a5622e23b6cc15
Leonardo Vidarte <lvidarte@gmail.com>
2013-07-19 10:52:10 -0300

    First usable version of board

--- plotter/board.py
+++ plotter/board.py
@@ -8,11 +8,41 @@ import math
 STEPS_PER_MM = 51.2 # asumo 512 steps por cm
 
 
+def drange(start, stop, step=1):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+
 class Point(object):
     def __init__(self, x, y, id=None):
         self.x = x
         self.y = y
         self.id = id
+
+    def get_points_from(self, point):
+        opposite = self.y - point.y
+        adjacent = self.x - point.x
+        triangle = Triangle(opposite, adjacent)
+        points = []
+        for hypotenuse in drange(1, int(triangle.hypotenuse), 0.5):
+            x = point.x + (hypotenuse * triangle.cos)
+            y = point.y + (hypotenuse * triangle.sin)
+            points.append(Point(x, y))
+        return points
+
+    def __repr__(self):
+        return "(%.2f, %.2f)" % (self.x, self.y)
+
+
+class Triangle:
+    def __init__(self, opposite, adjacent):
+        self.opposite = opposite
+        self.adjacent = adjacent
+        self.hypotenuse = math.hypot(opposite, adjacent)
+        self.sin = opposite / self.hypotenuse
+        self.cos = adjacent / self.hypotenuse
 
 
 class Board(object):
@@ -67,28 +97,37 @@ class App(tk.Frame):
         self.canvas.bind_all('<Button-1>', self._add_point)
 
     def init_board(self):
-        self.add_point(self.board.width / 2, 100, 'red')
+        point = Point(self.board.width / 2, 100)
+        self.add_point(point, 'red')
 
     def _add_point(self, event):
-        self.add_point(event.x, event.y)
+        point = Point(event.x, event.y)
+        for p in point.get_points_from(self.board.points[-1]):
+            self.add_point(p)
+        self.add_point(point)
 
-    def add_point(self, x, y, color='black'):
-        id = self.draw_point(x, y, color)
-        point = Point(x, y, id)
+    def add_point(self, point, color='black'):
+        point.id = self.draw_point(point, color)
         self.board.add_point(point)
         self.update_status(point)
 
-    def draw_point(self, x, y, color):
-        return self.canvas.create_rectangle(x, y, x + 2, y + 2,
-                                            width=0, fill=color)
+    def draw_point(self, point, color):
+        return self.canvas.create_rectangle(
+            point.x, point.y,
+            point.x + 1, point.y + 1,
+            width=0, fill=color)
 
     def update_status(self, point):
+        text = self.get_status_text(point)
+        print(text)
+        self.status.config(text=text)
+
+    def get_status_text(self, point):
         h0, h1 = self.board.get_hypot(point)
         s0, s1 = self.board.get_relative_steps(point)
         args = (point.x, point.y, h0, h1, s0, s1)
-        text = "Point at (%d, %d) :: Hypots (%.2f, %.2f) :: Relative steps (%d, %d)" % args
-        print text
-        self.status.config(text=text)
+        str = "Point (%d, %d) :: Hypots (%.2f, %.2f) :: Relative steps (%d, %d)"
+        return str % args
 
 
 if __name__ == '__main__':

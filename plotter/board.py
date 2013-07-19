7a5e5fb3ab9190fa6d9fc9f3ed11f252000543f3
Leonardo Vidarte <lvidarte@gmail.com>
2013-07-19 13:23:57 -0300

    Refactoring on board

--- plotter/board.py
+++ plotter/board.py
@@ -16,19 +16,21 @@ def drange(start, stop, step=1):
 
 
 class Point(object):
-    def __init__(self, x, y, id=None):
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.id = id
 
     def get_points_from(self, point):
         opposite = self.y - point.y
         adjacent = self.x - point.x
-        triangle = Triangle(opposite, adjacent)
+        hypot = math.hypot(opposite, adjacent)
+        sin = opposite / hypot
+        cos = adjacent / hypot
         points = []
-        for hypotenuse in drange(1, int(triangle.hypotenuse), 0.5):
-            x = point.x + (hypotenuse * triangle.cos)
-            y = point.y + (hypotenuse * triangle.sin)
+        for h in drange(1, int(hypot), 0.5):
+            x = point.x + (h * cos)
+            y = point.y + (h * sin)
             points.append(Point(x, y))
         return points
 
@@ -36,23 +38,36 @@ class Point(object):
         return "(%.2f, %.2f)" % (self.x, self.y)
 
 
-class Triangle:
-    def __init__(self, opposite, adjacent):
-        self.opposite = opposite
-        self.adjacent = adjacent
-        self.hypotenuse = math.hypot(opposite, adjacent)
-        self.sin = opposite / self.hypotenuse
-        self.cos = adjacent / self.hypotenuse
-
-
 class Board(object):
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.points = []
+        self.canvas = None
 
     def add_point(self, point):
+        if self.points:
+            last_point = self.points[-1]
+            for p in point.get_points_from(last_point):
+                self._add_point(p)
+        self._add_point(point)
+
+    def _add_point(self, point):
         self.points.append(point)
+        if self.canvas:
+            self.draw_point(point)
+        print(self.get_log(point))
+
+    def draw_point(self, point):
+        if self.points:
+            color = 'black'
+        else:
+            color = 'red'
+        self.canvas.create_rectangle(
+            point.x, point.y,
+            point.x + 1, point.y + 1,
+            width=0, fill=color)
 
     def get_hypot(self, point):
         h0 = math.hypot(point.x, point.y)
@@ -75,6 +90,13 @@ class Board(object):
             s0, s1 = 0, 0
         return (s0, s1)
 
+    def get_log(self, point):
+        h0, h1 = self.get_hypot(point)
+        s0, s1 = self.get_relative_steps(point)
+        args = (point, h0, h1, s0, s1)
+        str = "Point %s :: Hypots (%.2f, %.2f) :: Relative steps (%d, %d)"
+        return str % args
+
 
 class App(tk.Frame):
 
@@ -94,54 +116,33 @@ class App(tk.Frame):
         self.status.grid()
 
     def create_events(self):
-        self.canvas.bind_all('<Button-1>', self._add_point)
+        self.canvas.bind_all('<Button-1>', self.add_point)
 
     def init_board(self):
+        self.board.canvas = self.canvas
         point = Point(self.board.width / 2, 100)
-        self.add_point(point, 'red')
-
-    def _add_point(self, event):
-        point = Point(event.x, event.y)
-        for p in point.get_points_from(self.board.points[-1]):
-            self.add_point(p)
-        self.add_point(point)
-
-    def add_point(self, point, color='black'):
-        point.id = self.draw_point(point, color)
         self.board.add_point(point)
         self.update_status(point)
 
-    def draw_point(self, point, color):
-        return self.canvas.create_rectangle(
-            point.x, point.y,
-            point.x + 1, point.y + 1,
-            width=0, fill=color)
+    def add_point(self, event):
+        point = Point(event.x, event.y)
+        self.board.add_point(point)
+        self.update_status(point)
 
     def update_status(self, point):
-        text = self.get_status_text(point)
-        print(text)
+        text = self.board.get_log(point)
         self.status.config(text=text)
-
-    def get_status_text(self, point):
-        h0, h1 = self.board.get_hypot(point)
-        s0, s1 = self.board.get_relative_steps(point)
-        args = (point.x, point.y, h0, h1, s0, s1)
-        str = "Point (%d, %d) :: Hypots (%.2f, %.2f) :: Relative steps (%d, %d)"
-        return str % args
 
 
 if __name__ == '__main__':
-
-    prog_name = u'Plotter'
-
     from optparse import OptionParser
+    prog_name = u'Plotter'
     parser = OptionParser(description=prog_name)
     parser.add_option('-W', '--width', type=int, default=640,
                       help="board width")
     parser.add_option('-H', '--height', type=int, default=480,
                       help="board height")
     args, _ = parser.parse_args()
-
     app = App(args.width, args.height)
     app.master.title(prog_name)
     app.mainloop()
